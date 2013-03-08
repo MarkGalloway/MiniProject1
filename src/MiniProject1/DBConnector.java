e567255bf31d6041fe1109f9e4a963fc6241efb8
Mark Galloway <mgallowa@ualberta.ca>
2013-03-08 10:54:22 -0700

    listreview now returns ArrayList. Added length checks to methods.

--- src/MiniProject1/DBConnector.java
+++ src/MiniProject1/DBConnector.java
@@ -6,6 +6,7 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 
 /*
  * Handles the nitty gritty syntax of interacting with the DB
@@ -58,11 +59,6 @@ public class DBConnector {
      * Closes the connection to the Oracle DB
      */
     public void closeConnection() {
-        
-        if (connection == null) {
-           return;
-        }
-        
         try {
             //close the connection
             connection.close();
@@ -73,26 +69,34 @@ public class DBConnector {
     }
     
     /*
-     * Lists the reviews for the user corresponding to argument 'username' which have been written 
+     * Takes as input a 'username' and returns an ArrayList of the reviews corresponding 'username' which have been written 
      * since his/her last login, ordered based on date with more recent reviews listed first.
      * 
      * TODO:
-     *  Unsure How I should be printing format of last reviews. just doing "date: xx reviewer: yy Reviewtext: zz"
+     *  Unsure what the format of last reviews should be. just doing "date: xx reviewer: yy Reviewtext: zz"
      *  for now... this may need to be changed.
      * 
      */
-    public void listReviews(String username) {
+    public ArrayList<String> listReviews(String username) {
+        
+        //input length conversion
+        String usr = stringChop(username,20);
+        
+        //query to get the users reviews which are new since last login
         String query = "select *" +
                       " from reviews R, users U" +
-                      " where U.email = "+ "'" + username + "'" + 
+                      " where U.email = "+ "'" + usr + "'" + 
                       " and  R.reviewee = U.email" +
-                      " and R.rdate > U.last_login";
+                      " and R.rdate > U.last_login" +
+                      " order by R.rdate DESC";
+        
+        ArrayList<String> out = new ArrayList<String>();
         
         try {
             ResultSet rs = stmt.executeQuery(query);
             
             while(rs.next()){
-                System.out.println("Date: " + rs.getDate(6) +
+                out.add("Date: " + rs.getDate(6) +
                 		", Reviewer: " + (rs.getString(4)).trim() +
                 		", Review Text: " + rs.getString(3));
             }
@@ -100,17 +104,22 @@ public class DBConnector {
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
+        return out;
     }
     
     /*
      * Updates the login_date of the user specified in 'username'
      * to the current system date.
      * 
+     * TODO: Error checks or return value?? Not sure if needed
      */
     public void updateLoginDate(String username){
         
+        //input length conversion
+        String usr = stringChop(username,20);
+        
         //the query to be passed in
-        String query = "Select email,last_login from users where email = " + "'" + username +"'";
+        String query = "Select email,last_login from users where email = " + "'" + usr +"'";
         
         //the new current date
         java.sql.Date sqlDate = new java.sql.Date(new java.util.Date().getTime());
@@ -135,7 +144,12 @@ public class DBConnector {
      *  Returns true if the user exists and password matches
      *  Returns false if either user not found or password is wrong
      */
-    public boolean verifyUser(String username, String pwd) {
+    public boolean verifyUser(String username, String password) {
+        
+        //input length conversion
+        String usr = stringChop(username,20);
+        String pwd = stringChop(password,4);
+        
         //query to get all users
         String query ="select * from users";
         
@@ -145,7 +159,7 @@ public class DBConnector {
             
             //loop through the results
             while (rs.next()) {
-                if(rs.getString("email").trim().equalsIgnoreCase(username)  //TODO: is ignorecase right here??
+                if(rs.getString("email").trim().equalsIgnoreCase(usr)  //TODO: is ignorecase right here??
                         && rs.getString("pwd").trim().equalsIgnoreCase(pwd)) { //TODO: is ignorecase right here??
                     //user matches username && pswd
                     return true;
@@ -163,6 +177,10 @@ public class DBConnector {
      *  Returns true if the user exists
      */
     public boolean existsUser(String username) {
+        
+        //input length conversion
+        String usr = stringChop(username,20);
+        
         //query to select primary key of all users
         String query ="select email from users";
         
@@ -171,7 +189,7 @@ public class DBConnector {
             ResultSet rs = stmt.executeQuery(query);
             //loop through results
             while (rs.next()) {
-                if(rs.getString("email").trim().equalsIgnoreCase(username)) { //TODO: is ignorecase right here??
+                if(rs.getString("email").trim().equalsIgnoreCase(usr)) { //TODO: is ignorecase right here??
                     //user found
                     return true;
                 }
@@ -193,10 +211,15 @@ public class DBConnector {
      * returns false if user already exists
      * 
      */
-    public boolean addNewUser(String username, String name, String pwd) {        
+    public boolean addNewUser(String username, String nameString, String password) {        
+        
+        //input length conversion
+        String usr = stringChop(username,20);
+        String pwd = stringChop(password,4);
+        String name = stringChop(nameString,20);
         
         //if user already exists, then reject.
-        if(existsUser(username)) {
+        if(existsUser(usr)) {
             return false;
         }
         
@@ -211,7 +234,7 @@ public class DBConnector {
             //Insert the new user as a row in the table
             rs.moveToInsertRow();
             //Insert values into new row
-            rs.updateString("email", username);
+            rs.updateString("email", usr);
             rs.updateString("name",name);
             rs.updateString("pwd", pwd);
             rs.updateDate("last_login", sqlDate);
@@ -223,6 +246,19 @@ public class DBConnector {
         }
         
         return true;
+    }
+    
+    /*
+     * Takes an input string and a max.
+     * If the string is shorter than max, it is returned.
+     * If the string is longer than max, it is shortened to be
+     * max chars long. Simply cuts off the end of strings that are
+     * too long for the DB.
+     */
+    private static String stringChop(String s, int max) {
+        int maxLength = (s.length() < max) ? s.length() 
+                                           : max;
+        return s.substring(0, maxLength);
     }
     
     public String getJdbcURL() {
