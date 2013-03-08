bf41e8a6e291987dbb60da545d650f476fe43d2d
Mark Galloway <mgallowa@ualberta.ca>
2013-03-08 02:28:02 -0700

    Adding users, verifying users, print reviews, update login_date
    Moved create/drop statements to Tests package, as they should not be
    created in our final product

--- src/MiniProject1/DBConnector.java
+++ src/MiniProject1/DBConnector.java
@@ -9,13 +9,14 @@ import java.sql.Statement;
 
 /*
  * Handles the nitty gritty syntax of interacting with the DB
+ * so we don't need try/catch all over the place in the UI loop
  */
 public class DBConnector {
 
     private static String mDriverName = "oracle.jdbc.driver.OracleDriver";
     private static Class drvClass = null;
     private static Connection connection = null;
-    private Statement stmt = null;
+    public Statement stmt = null;
     private String jdbcURL;
     private String jdbcUserName;
     private String jdbcPassword;
@@ -72,111 +73,157 @@ public class DBConnector {
     }
     
     /*
-     * Drops the hard coded tables
-     */
-    public void dropTables() {
-        try {   
-            stmt.executeUpdate("drop table purchases");
-            stmt.executeUpdate("drop table offers");
-            stmt.executeUpdate("drop table ads");
-            stmt.executeUpdate("drop table reviews");
-            stmt.executeUpdate("drop table users");
-            stmt.executeUpdate("drop table categories");
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
-    }
-    
-    /*
-     * Creates the hard coded tables
-     */
-    public void createTables() {
-        String categories = "create table categories " +
-                "(cat char(10)," +
-                "supercat char(10)," +
-                "primary key (cat)," +
-                "foreign key (supercat) references categories)";
-        String users = "create table users " +
-                "(email char(20)," + 
-                "name char(20)," +
-                "pwd char(4)," +
-                "last_login date," +
-                "primary key (email))";
-        String reviews = "create table reviews " +
-                "(rno int," +
-                "rating int," +
-                "text char(80)," +
-                "reviewer char(20)," +
-                "reviewee char(20)," +
-                "rdate date," +
-                "primary key (rno)," +
-                "foreign key (reviewer) references users," +
-                "foreign key (reviewee) references users)";
-        String offers = "create table offers " +
-                "(ono int," +
-                "ndays int," +
-                "price float," +
-                "primary key (ono))";
-        String ads = "create table ads " +
-                "(aid char(4)," +
-                "atype char(1) check (atype='S' OR atype='W')," +
-                "title char(20)," +
-                "price int," +
-                "descr char(40)," +
-                "location char(15)," +
-                "pdate date," +
-                "cat char(10)," +
-                "poster char(20)," +
-                "primary key (aid)," +
-                "foreign key (cat) references categories," +
-                "foreign key (poster) references users)";
-        String purchases ="create table purchases " +
-                "(pur_id char(4)," +
-                "start_date date," +
-                "aid char(4)," +
-                "ono int," +
-                "primary key (pur_id)," +
-                "foreign key (aid) references ads," +
-                "foreign key (ono) references offers)";
-        
-        try {
-            stmt.executeUpdate(categories);
-            stmt.executeUpdate(users);
-            stmt.executeUpdate(reviews);
-            stmt.executeUpdate(offers);
-            stmt.executeUpdate(ads);
-            stmt.executeUpdate(purchases);
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
-    }
-
-    /*
      * Lists the reviews for the user corresponding to argument 'username' which have been written 
      * since his/her last login, ordered based on date with more recent reviews listed first.
+     * 
+     * TODO:
+     *  Unsure How I should be printing format of last reviews. just doing "date: xx reviewer: yy Reviewtext: zz"
+     *  for now... this may need to be changed.
+     * 
      */
     public void listReviews(String username) {
-        //TODO
-        System.out.println("listReview called, but not yet implemented");
-        return;
-    }
-
-    /*
-     * inserts the statement in string S
-     * HAS ERRORS
-     */
-    public void insert(String s) {
-        
-        //add some sort of empty string check
-        // cause it throws on s = "";
+        String query = "select *" +
+                      " from reviews R, users U" +
+                      " where U.email = "+ "'" + username + "'" + 
+                      " and  R.reviewee = U.email" +
+                      " and R.rdate > U.last_login";
         
         try {
-            stmt.executeUpdate(s);
+            ResultSet rs = stmt.executeQuery(query);
+            
+            while(rs.next()){
+                System.out.println("Date: " + rs.getDate(6) +
+                		", Reviewer: " + (rs.getString(4)).trim() +
+                		", Review Text: " + rs.getString(3));
+            }
+            
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
     }
     
+    /*
+     * Updates the login_date of the user specified in 'username'
+     * to the current system date.
+     * 
+     */
+    public void updateLoginDate(String username){
+        
+        //the query to be passed in
+        String query = "Select email,last_login from users where email = " + "'" + username +"'";
+        
+        //the new current date
+        java.sql.Date sqlDate = new java.sql.Date(new java.util.Date().getTime());
+        
+        try {
+            //get the resultset from query
+            ResultSet rs = stmt.executeQuery(query);
+            //move to the row
+            rs.absolute(1);
+            //update the date
+            rs.updateDate("last_login", sqlDate);
+            //update the change in the database
+            rs.updateRow();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        
+    }
+    
+    /*
+     *  Checks the users table to see if the user exists and the password matches
+     *  Returns true if the user exists and password matches
+     *  Returns false if either user not found or password is wrong
+     */
+    public boolean verifyUser(String username, String pwd) {
+        //query to get all users
+        String query ="select * from users";
+        
+        try {
+            //execute the query
+            ResultSet rs = stmt.executeQuery(query);
+            
+            //loop through the results
+            while (rs.next()) {
+                if(rs.getString("email").trim().equalsIgnoreCase(username)  //TODO: is ignorecase right here??
+                        && rs.getString("pwd").trim().equalsIgnoreCase(pwd)) { //TODO: is ignorecase right here??
+                    //user matches username && pswd
+                    return true;
+                }
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        //no match, user does not exist
+        return false;
+    }
+    
+    /*
+     *  Checks the users table to see if the user exists
+     *  Returns true if the user exists
+     */
+    public boolean existsUser(String username) {
+        //query to select primary key of all users
+        String query ="select email from users";
+        
+        try {
+            //execute query
+            ResultSet rs = stmt.executeQuery(query);
+            //loop through results
+            while (rs.next()) {
+                if(rs.getString("email").trim().equalsIgnoreCase(username)) { //TODO: is ignorecase right here??
+                    //user found
+                    return true;
+                }
+            }
+            
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        //user not found
+        return false;
+    }
+    
+    /*
+     * Adds a new user to the Users table.
+     * 
+     * TODO: Verify correct input ??
+     * 
+     * returns true if user added successfully
+     * returns false if user already exists
+     * 
+     */
+    public boolean addNewUser(String username, String name, String pwd) {        
+        
+        //if user already exists, then reject.
+        if(existsUser(username)) {
+            return false;
+        }
+        
+        //query to return the users table
+        String query = "select email,name,pwd,last_login from users";
+        //the current date
+        java.sql.Date sqlDate = new java.sql.Date(new java.util.Date().getTime());
+        
+        try {
+            //execute query
+            ResultSet rs = stmt.executeQuery(query);
+            //Insert the new user as a row in the table
+            rs.moveToInsertRow();
+            //Insert values into new row
+            rs.updateString("email", username);
+            rs.updateString("name",name);
+            rs.updateString("pwd", pwd);
+            rs.updateDate("last_login", sqlDate);
+            //make permanent
+            rs.insertRow();
+            
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        
+        return true;
+    }
     
     public String getJdbcURL() {
         return jdbcURL;
