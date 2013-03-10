40b588e4bf503cf91695aa691df02d18fe49cd12
Mark Galloway <mgallowa@ualberta.ca>
2013-03-10 14:11:14 -0600

    User class for storing user data

---
+++ src/MiniProject1/User.java
@@ -0,0 +1,53 @@
+package MiniProject1;
+
+public class User {
+    private String name;
+    private String email;
+    private Integer numberOfAds;
+    private Double avgRating;
+    
+    public User(String name, String email, Integer numberOfAds, Double avgRating) {
+        super();
+        this.setName(name);
+        this.setEmail(email);
+        this.setNumberOfAds(numberOfAds);
+        this.setAvgRating(avgRating);
+    }
+
+    public String toString() {
+        return "Email: " + this.getEmail() + ", Name: " + this.getName() + 
+                ", Number of Ads: " + this.getNumberOfAds() + ", Average Rating: " + String.valueOf(((double)((int)(this.getAvgRating() * 10))) / 10);
+    }
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getNumberOfAds() {
+        return numberOfAds;
+    }
+
+    public void setNumberOfAds(Integer numberOfAds) {
+        this.numberOfAds = numberOfAds;
+    }
+
+    public Double getAvgRating() {
+        return avgRating;
+    }
+
+    public void setAvgRating(Double avgRating) {
+        this.avgRating = avgRating;
+    }
+}
